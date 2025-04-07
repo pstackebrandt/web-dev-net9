a0e5653d4afc8080809818911dee586245e1dea1
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-04-07 17:00:48 +0200

    Add unit tests for Entity Models in Northwind project
    - Created EntityModelTests.cs with tests for database connection,
      categories count, and product name retrieval.
    - Added Northwind.UnitTests.csproj to manage test dependencies and
      project settings.
    - p. 41

---
+++ code/MatureWeb/Northwind.UnitTests/EntityModelTests.cs
@@ -0,0 +1,33 @@
+﻿using Northwind.EntityModels;
+
+namespace Northwind.UnitTests;
+
+
+public class EntityModelTests
+{
+    [Fact]
+    public void DatabaseConnectTest()
+    {
+        using NorthwindContext db = new();
+        Assert.True(db.Database.CanConnect());
+    }
+
+    [Fact]
+    public void CategoriesCountTest()
+    {
+        using NorthwindContext db = new();
+        int expected = 8;
+        int actual = db.Categories.Count();
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ProductId1IsChaiTest()
+    {
+        using NorthwindContext db = new();
+        string expected = "Chai";
+        Product? product = db.Products.Find(keyValues: 1);
+        string actual = product?.ProductName ?? string.Empty;
+        Assert.Equal(expected, actual);
+    }
+}

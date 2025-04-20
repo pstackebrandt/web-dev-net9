f83e29607b329275564013b8cd52e4f5e9a17da5
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-04-20 22:15:32 +0200

    Add initial project ExploringBootstrap (MVC)

---
+++ code/MatureWeb/ExploringBootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExploringBootstrap.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
